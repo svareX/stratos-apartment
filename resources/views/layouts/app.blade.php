38c162e0dd9bf496c06ea3f653e3dd227c547cc5
Štěpán Švarc <stepanecek2003@gmail.com>
2026-03-18 10:05:44 +0100

    Navigation menu setup + HomeController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,21 +16,15 @@
     <body class="font-sans antialiased">
 
         <div class="min-h-screen">
+            <!-- Navigation -->
+            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
+            <!-- Main Page Content -->
             <main class="bg-cream">
                 {{ $slot }}
             </main>
 
+            <!-- Footer -->
             @include('layouts.footer')
         </div>
 
