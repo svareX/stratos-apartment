0d62c9713fa9c9f6d420719bd73af7be65f16ca0
Štěpán Švarc <stepanecek2003@gmail.com>
2026-04-27 19:55:27 +0200

    Admin reservation list tabs

--- app/Filament/Resources/Reservations/Pages/ListReservations.php
+++ app/Filament/Resources/Reservations/Pages/ListReservations.php
@@ -5,10 +5,29 @@ namespace App\Filament\Resources\Reservations\Pages;
 use App\Filament\Resources\Reservations\ReservationResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class ListReservations extends ListRecords
 {
     protected static string $resource = ReservationResource::class;
+
+
+    public function getTabs(): array
+    {
+        $start = Carbon::now()->addMonth()->startOfMonth()->toDateString();
+        $end = Carbon::now()->addMonth()->endOfMonth()->toDateString();
+
+        return [
+            __('All') => Tab::make(),
+
+            __('Next month') => Tab::make()
+                ->modifyQueryUsing(static function (Builder $query) use ($start, $end) {
+                    $query->whereBetween('check_in', [$start, $end]);
+                }),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
